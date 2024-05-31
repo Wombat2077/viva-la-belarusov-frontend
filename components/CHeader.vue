c3f024d617a2e7588ba9ff607b6a4e7a14e368c4
wombat1742 <tomgogard@gmail.com>
2024-05-31 19:14:48 +0300

    добавлены векторные картинки, обновлен Cheader, добавлен registry.vue

--- components/CHeader.vue
+++ components/CHeader.vue
@@ -1,35 +1,17 @@
 <template>
     
     <div class="flex spacex-2 bg-secondarybg sm:min-h-20 sm:min-w-full p-5 text-white">
-        <div class="mr-10 hover:bg-secondarybglight flex justify-center items-center -my-5 -ml-5 p-2">
+        <div class="mr-10 hover:bg-secondarybglight sm:min-w-20 flex justify-center items-center -my-5 -ml-5 p-2" v-show="showReturnArrow">
             <a>
-                <svg width="43" height="46" viewBox="0 0 43 46" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g filter="url(#filter0_d_38_483)">
-                    <path d="M5.23943 17.2322C4.26313 18.2085 4.26313 19.7915 5.23943 20.7678L21.1493 36.6777C22.1256 37.654 23.7086 37.654 24.6849 36.6777C25.6612 35.7014 25.6612 34.1184 24.6849 33.1421L10.5427 19L24.6849 4.85786C25.6612 3.88155 25.6612 2.29864 24.6849 1.32233C23.7086 0.34602 22.1256 0.34602 21.1493 1.32233L5.23943 17.2322ZM39.0072 16.5L7.0072 16.5V21.5L39.0072 21.5V16.5Z" fill="white"/>
-                    </g>
-                    <defs>
-                    <filter id="filter0_d_38_483" x="0.507202" y="0.590088" width="42.5" height="44.8198" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-                    <feOffset dy="4"/>
-                    <feGaussianBlur stdDeviation="2"/>
-                    <feComposite in2="hardAlpha" operator="out"/>
-                    <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.25 0"/>
-                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_38_483"/>
-                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_38_483" result="shape"/>
-                    </filter>
-                    </defs>
-                </svg>
+                <img src="@/assets/svg/return-arrow.svg">
             </a>
         </div>
-        <svg width="45" height="45" viewBox="0 0 45 45" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect x="34" y="34" width="11" height="11" fill="white"/>
-            <rect x="34" y="17" width="11" height="11" fill="white"/>
-            <rect opacity="0.5" x="17" y="17" width="11" height="11" fill="white"/>
-            <rect opacity="0.5" x="34" width="11" height="11" fill="white"/>
-            <rect x="17" width="11" height="11" fill="white"/>
-            <rect width="11" height="11" fill="white"/>
-        </svg>
+        <img src="@/assets/svg/logo.svg">
         <div class="mx-5 mt-2 text-xl font-semibold"> VIVA EXPRESS</div>
     </div>
 </template>
+<script setup>
+    defineProps({
+        showReturnArrow : Boolean
+    })
+</script>
