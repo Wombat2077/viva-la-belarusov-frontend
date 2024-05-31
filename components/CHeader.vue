e2f6723b37ec2baa69aa2f5637e104986d646b84
wombat1742 <tomgogard@gmail.com>
2024-05-31 22:11:02 +0300

    обновлен Cheader.vue

--- components/CHeader.vue
+++ components/CHeader.vue
@@ -8,10 +8,28 @@
         </div>
         <img src="@/assets/svg/logo.svg">
         <div class="mx-5 mt-2 text-xl font-semibold"> VIVA EXPRESS</div>
+        <div class="flex items-center ml-auto hover:bg-secondarybglight -my-5 mr-2 p-5" @click="toggle"> <img src="@/assets/svg/anon-avatar.svg"></div>
+        <Menu ref="profileMenu"  :model="unAuthOptions" :popup="true" class="text-white bg-secondarybg -translate-x-4" :pt="{menuitem : 'hover:bg-secondarybglight p-2'}"/>
+            
     </div>
 </template>
 <script setup>
+    import Menu from "primevue/menu";
+    import { ref } from "vue";
+    const profileMenu = ref();
+    const unAuthOptions = ref([
+        {
+            label: "Войти",
+        },
+        {
+            label: "Зарегистрироваться",
+        }
+    ]);
+    function toggle(event){
+        profileMenu.value.toggle(event);
+    }
     defineProps({
         showReturnArrow : Boolean
+
     })
 </script>
