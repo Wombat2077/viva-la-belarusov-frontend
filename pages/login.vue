b86726581403c3a3f5808c7b9a009a48cb10c77f
wombat1742 <tomgogard@gmail.com>
2024-05-31 12:48:32 +0300

    создан проект

---
+++ pages/login.vue
@@ -0,0 +1,18 @@
+<style scoped>
+
+</style>
+
+<template>
+    <div class="login-form">
+        <label for="login-box">Введите ваш телефон, e-mail или логин</label>
+        <InputText id="login-box" type="text" v-model="login" placeholder="Телефон/e-mail/логин" />
+        <label for="login-box">Введите ваш телефон, e-mail или логин</label>
+        <Password id="login-box" type="text" v-model="login" placeholder="Телефон/e-mail/логин" />
+    </div>
+
+</template>
+<script setup>
+ import Password from "primevue/password";
+ import InputText from "primevue/inputtext";
+
+</script>
